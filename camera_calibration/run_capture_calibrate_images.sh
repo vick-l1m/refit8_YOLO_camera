4da33661feb2cc8ace9cccf767402a06147f5098
victor <z5482422@gmail.com>
2026-02-26 19:09:00 +1100

    Errors in calibration step

--- camera_calibration/run_capture_calibrate_images.sh
+++ camera_calibration/run_capture_calibrate_images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export PYTHONPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 python3 capture_calibration_images.py --autofocus \
   --interval 3 \
   --width 1920 \
